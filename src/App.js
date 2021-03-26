001a5da902759a45631d96e3fed4fee9347fbd6f
cristian <cristian.ortiz@uda.cl>
2021-03-25 22:39:13 -0300

    small app to query and API to return text quotes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Quote from "./component/Quote";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+  //useState hook for the quote
+  const [quote, handleQuote] = useState({});
+
+  const requestAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const quote = await api.json();
+    handleQuote(quote[0]);
+  };
+  //useEffect para cargar una frase, asi al recargar navegador siempre habra una frase
+  useEffect(() => {
+    requestAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Quote quote={quote} />
+      <Button onClick={requestAPI}>Get Quote</Button>
+    </Container>
   );
 }
 
